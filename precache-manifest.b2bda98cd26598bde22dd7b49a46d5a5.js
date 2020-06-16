8fb29566040ea901a4b404980f923e581ef5a9d7
gyanendraknojiya <gyanendrak064@gmail.com>
2020-06-17 00:28:31 +0530

    Updates

---
+++ precache-manifest.b2bda98cd26598bde22dd7b49a46d5a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6e79e583c6c4c4b07b0a1b7d64ab57",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b599c5c3d56f75f04d9c",
+    "url": "/monsters-rolodex/static/css/main.6bd2acb2.chunk.css"
+  },
+  {
+    "revision": "ed9f4125c6ddc10185a3",
+    "url": "/monsters-rolodex/static/js/2.a281a20f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a281a20f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b599c5c3d56f75f04d9c",
+    "url": "/monsters-rolodex/static/js/main.03dc3c5c.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
