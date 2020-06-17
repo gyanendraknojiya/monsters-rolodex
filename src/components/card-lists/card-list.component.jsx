cc615758137ad293d0cadbf305d6448901d4c5f6
gyanendraknojiya <gyanendrak064@gmail.com>
2020-06-17 14:22:07 +0530

    added

--- src/components/card-lists/card-list.component.jsx
+++ src/components/card-lists/card-list.component.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import './card-list.style.css'
-import Card from '../card/card.component';
+import React from "react";
+import "./card-list.style.css";
+import Card from "../card/card.component";
 
-
-function CardList(props){
-    return (
-        <div className='cardlist'>
-            {props.monsters.map(Element=><Card key={Element.id} monsters={Element}/>)}</div>
-    )
+function CardList(props) {
+  return (
+    <div className="cardlist">
+      {props.monsters.map((Element) => (
+        <Card key={Element.id} monsters={Element} />
+      ))}
+    </div>
+  );
 }
 
 export default CardList;
